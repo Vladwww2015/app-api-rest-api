3696dcb64cce82f3ded3d6eb0fc3d6229e497bdd
Yurchenko <iwwwi2015@gmail.com>
2024-02-21 20:20:35 -0500

    v1.0.26

--- src/Http/Resources/V1/Admin/Catalog/ProductCustomerGroupPriceResource.php
+++ src/Http/Resources/V1/Admin/Catalog/ProductCustomerGroupPriceResource.php
@@ -23,10 +23,10 @@ class ProductCustomerGroupPriceResource extends JsonResource
             'qty' => $this->qty,
             'value' => $this->value,
             'value_type' => $this->value_type,
-            'product_sku' => PreloadProduct::getSkuByProductId($this->product_id),
-            'inventory_source_code' => PreloadInventorySource::getCodeById($this->inventory_source_id),
-            'customer_group_code' => PreloadCustomerGroup::getCodeById($this->customer_group_id),
-            'customer_group_name' => PreloadCustomerGroup::getNameById($this->customer_group_id),
+            'product_sku' => PreloadProduct::getSkuByProductId((int) $this->product_id),
+            'inventory_source_code' => PreloadInventorySource::getCodeById((int) $this->inventory_source_id),
+            'customer_group_code' => PreloadCustomerGroup::getCodeById((int) $this->customer_group_id),
+            'customer_group_name' => PreloadCustomerGroup::getNameById((int) $this->customer_group_id),
         ];
     }
 }
