c59ec471d800b00a8bb26c5c9f5cda9c23e5a365
Yurchenko <iwwwi2015@gmail.com>
2024-06-28 14:24:23 -0400

    v1.0.44

--- src/Http/Resources/V1/Admin/Catalog/ProductImageResource.php
+++ src/Http/Resources/V1/Admin/Catalog/ProductImageResource.php
@@ -17,8 +17,9 @@ class ProductImageResource extends JsonResource
         return [
             'id'                 => $this->id,
             'path'               => $this->path,
+            'is_main'            => (bool) $this->is_main,
             'url'                => $this->url,
-            'original_image_url' => $this->url,
+            'original_image_url' => url($this->url),
             'small_image_url'    => url('cache/small/'.$this->path),
             'medium_image_url'   => url('cache/medium/'.$this->path),
             'large_image_url'    => url('cache/large/'.$this->path),
